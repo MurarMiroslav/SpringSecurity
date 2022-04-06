c70407b974417889ad484c347804a35e19286fb5
Miroslav <mmurar@scoop-software.cz>
2022-04-07 00:03:58 +0200

    Remember me - simple hash-based token =>cele remember me je ze pri refreshnuti stranky kedy vytvaranas novu SESSION tak mas aj dalsiu SESSION => cez COOKIE kde mas udaje a tato pretrvava

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,24 +1,28 @@
 package guru.sfg.brewery.config;
 
 import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Created by jt on 6/13/20.
  */
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     // needed for use with Spring Data JPA SPeL
     @Bean
@@ -46,15 +50,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             .defaultSuccessUrl("/")
                             .failureUrl("/?error");
                 })
-                .logout(logoutConfigurer -> {
-                    logoutConfigurer
-                            .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-                            .logoutSuccessUrl("/?logout")
-                            .permitAll();
-                })
-
+                 .logout(logoutConfigurer -> {
+                     logoutConfigurer
+                             .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                             .logoutSuccessUrl("/?logout")
+                             .permitAll();
+                 })
                 .httpBasic()
-                .and().csrf().ignoringAntMatchers("/h2-console/**", "/api/**");
+                .and().csrf().ignoringAntMatchers("/h2-console/**", "/api/**")
+                .and().rememberMe()
+                        .key("sfg-key")
+                        .userDetailsService(userDetailsService);
 
                 //h2 console config
                 http.headers().frameOptions().sameOrigin();
