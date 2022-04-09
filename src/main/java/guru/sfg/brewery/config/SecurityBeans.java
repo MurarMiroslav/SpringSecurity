a37b21aa41ded051839e83a93089fdcac9489dba
Miroslav <mmurar@scoop-software.cz>
2022-04-09 16:02:27 +0200

    CORS je to ze SERVER (applikacia) je napojena na druhy SERVER (applikaciu) a moze to byt nebezpecne... Toto osetruje hlavne BROWSER. My sme v tomto commite vypli SS v celom projekte a isli sme riesit 'specificku securitu'

--- src/main/java/guru/sfg/brewery/config/SecurityBeans.java
+++ src/main/java/guru/sfg/brewery/config/SecurityBeans.java
@@ -3,11 +3,13 @@ package guru.sfg.brewery.config;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 import com.warrenstrange.googleauth.GoogleAuthenticatorConfig;
 import com.warrenstrange.googleauth.ICredentialRepository;
+import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
@@ -21,12 +23,17 @@ import java.util.concurrent.TimeUnit;
 public class SecurityBeans {
 
     @Bean
+    PasswordEncoder passwordEncoder(){
+        return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
     public GoogleAuthenticator googleAuthenticator(ICredentialRepository credentialRepository){
         GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder configBuilder
                 = new GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder();
 
         configBuilder
-                .setTimeStepSizeInMillis(TimeUnit.SECONDS.toMillis(30))
+                .setTimeStepSizeInMillis(TimeUnit.SECONDS.toMillis(60))
                 .setWindowSize(10)
                 .setNumberOfScratchCodes(0);
 
